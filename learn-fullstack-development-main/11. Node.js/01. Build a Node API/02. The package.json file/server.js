46b773cd939304cfc6077cc992c2d7ff7a7b1fff
Nikhil Kumar <nikhilkumar.ise27@gmail.com>
2026-02-07 18:52:37 +0530

    start of Node.js

--- learn-fullstack-development-main/11. Node.js/01. Build a Node API/02. The package.json file/server.js
+++ learn-fullstack-development-main/11. Node.js/01. Build a Node API/02. The package.json file/server.js
@@ -1,1 +1,9 @@
-console.log('Hello Node!')
+// http module 
+import http from 'node:http'
+
+const PORT = 8000
+const server = http.createServer((req,res)=>{ //req -> request object ||| res-> response object
+    res.end('Hello from the server')
+})
+
+server.listen(PORT, ()=> console.log(`server running on port: ${PORT}`))

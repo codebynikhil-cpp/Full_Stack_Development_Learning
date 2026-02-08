13df27f424aa6cabfec70b2d6937b85d3ccf02ab
Nikhil Kumar <nikhilkumar.ise27@gmail.com>
2026-02-08 12:31:27 +0530

    setting a server

--- learn-fullstack-development-main/11. Node.js/02. Build a Fullstack Node App/02. Setting up the project/Exercise 2/server.js
+++ learn-fullstack-development-main/11. Node.js/02. Build a Fullstack Node App/02. Setting up the project/Exercise 2/server.js
@@ -1,10 +1,8 @@
-/*
-Challenge: 
-1. Initialise a nodejs project:
-	Name: “from-the-other-side”.
-    Description: “A platform for sharing ghostly encounters”.
-
-2. Enable modular js (in package.json).
-
-hint.md for help
-*/
+import http from 'node:http';
+const PORT = 8000
+const server = http.createServer((req,res) =>{
+    res.statusCode = 200
+    res.setHeader('Content-Type','text/html')
+    res.end('<html><h1>The server is working</h1></html>')
+})
+server.listen(PORT, ()=> console.log(`Connected to the port ${PORT}`));
